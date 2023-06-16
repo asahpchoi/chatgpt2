880fa64d061b9dc4eed04d3b4f147c019d0869b6
gpt chat <chatgpthkasa@gmail.com>
2023-06-16 17:36:39 +0000

    Update Audio2.js

--- src/comps/Audio2.js
+++ src/comps/Audio2.js
@@ -2,19 +2,27 @@ import { useWhisper } from "@chengsokdara/use-whisper";
 import env from "./env.json";
 import Button from "@mui/material/Button";
 import Backdrop from "@mui/material/Backdrop";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import * as React from "react";
 
 const Audio2 = ({ setPrompt }) => {
+  const [language, setLanguage] = React.useState("en");
   const { transcript, recording, startRecording, stopRecording } = useWhisper({
     apiKey: env.apiKey, // YOUR_OPEN_AI_TOKEN
     streaming: true,
-    timeSlice: 1_000, // 1 second
+    timeSlice: 1000, // 1 second
+    removeSilence: true,
     whisperConfig: {
-      language: "en",
+      language,
     },
   });
+
   return (
     <>
       <Button
+        variant="outlined"
         onClick={() => {
           startRecording();
         }}
@@ -23,12 +31,17 @@ const Audio2 = ({ setPrompt }) => {
       </Button>
 
       <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{
+          color: "#fff",
+          backgroundColor: "#000",
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}
         open={recording}
         onClick={() => {}}
       >
         {transcript.text}
         <Button
+          variant="outlined"
           onClick={() => {
             stopRecording();
             setPrompt(transcript.text);
